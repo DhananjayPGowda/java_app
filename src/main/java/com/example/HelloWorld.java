933218a605da9cf7328c888b50039bb9ef66d196
dhananjay <dhananjaypgowda8110@gmail.com>
2025-11-20 07:13:06 +0000

    java update

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,10 +1,16 @@
 package com.example;
-
+import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args){ 
         System.out.println("My Java application is running!");
+	Scanner sc = new Scanner(System.in);
         while (true) {
+
+	    System.out.print("Enter your something: ");
+            String data = sc.nextLine();
+	    System.out.println("you entered : " + data);
+
             try {
                 Thread.sleep(10000); 
             } catch (InterruptedException e) {
